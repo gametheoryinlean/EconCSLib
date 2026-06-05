3d8b9ebf1ca5b3fafe3a8ebf3c6fec46c61fd513
Zhan JING <zhan.jing@etu.minesparis.psl.eu>
2026-06-05 17:57:27 +0800

    Formalize truthful submodular CA open problem

--- EconCSLib/OpenProblem.lean
+++ EconCSLib/OpenProblem.lean
@@ -5,6 +5,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 import EconCSLib.OpenProblem.EFXExistence
 import EconCSLib.OpenProblem.SubmodularWelfareDemandOracle
+import EconCSLib.OpenProblem.TruthfulSubmodularCA
 
 /-!
 # EconCSLib.OpenProblem
